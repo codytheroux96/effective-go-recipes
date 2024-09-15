24adc1afbc1f0002247a520d39173dc223c4d3e0
codytheroux96 <cody.theroux3@gmail.com>
2024-09-15 16:39:34 -0400

    first recipe

--- reading_and_writing/io/marshal.go
+++ reading_and_writing/io/marshal.go
@@ -1,2 +1,25 @@
 package io
 
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"time"
+)
+
+type Ride struct {
+	ID       int
+	Time     time.Time
+	Duration time.Duration
+	Distance float64
+	Price    float64
+}
+
+func NewDecoder(r io.Reader) *Decoder {
+	return &Decoder{json.NewDecoder(r)}
+}
+
+func UnmarshalRide(data []byte, ride *Ride) error {
+	r := bytes.NewReader(data)
+	return NewDecoder(r).DecodeRide(ride)
+}
