1b407a11f84dcd09aa818c753b2f4026cf8ae5e8
codytheroux96 <cody.theroux3@gmail.com>
2024-09-16 21:42:31 -0400

    updated instructions with a solution tidbit

--- reading_and_writing/io/redirects/redirects.go
+++ reading_and_writing/io/redirects/redirects.go
@@ -3,6 +3,8 @@
 - Now you want the ability to check how many customers are still trying to access the old page
 - HTTP server log files are being saved in the logs diectory and some are compressed
 - You want to see how many HTTP redirect codes you have been throwing
+- ***Solution: write a function that gets an io.Reader and returns the total number of lines and the number of lines that have an
+HTTP redirect directive
 */
 
 package redirects
