d800a521113c812b92286aa5f951fc3170901d0f
codytheroux96 <cody.theroux3@gmail.com>
2024-09-16 21:39:59 -0400

    laid out the task for the next recipe

--- reading_and_writing/io/redirects/redirects.go
+++ reading_and_writing/io/redirects/redirects.go
@@ -1,1 +1,8 @@
+/*
+- Currently your company emits an HTTP redirect status code to send customers from an old webpage to a new one
+- Now you want the ability to check how many customers are still trying to access the old page
+- HTTP server log files are being saved in the logs diectory and some are compressed
+- You want to see how many HTTP redirect codes you have been throwing
+*/
+
 package redirects
