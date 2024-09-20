ba76d26b3418d204aa494c6f13f70eba6bbe2bb7
codytheroux96 <cody.theroux3@gmail.com>
2024-09-19 21:55:31 -0400

    added comments for next recipe

--- reading_and_writing/io/wtr/watr.go
+++ reading_and_writing/io/wtr/watr.go
@@ -1,1 +1,7 @@
+/*
+Assignment is to see if people are "cooking" accounting books by using Benfords Law which checks the distrubtion of leading digits in numbers
+- Law states that leading digits with smaller values occur more frequently than larger values
+- States that about 30% of numbers start with a 1 while less than 5% start with a 9
+*/
+
 package wtr
